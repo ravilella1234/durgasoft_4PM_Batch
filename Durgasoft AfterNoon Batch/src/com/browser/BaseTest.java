9c8cc246806e4d3238c64ec2472c69ad2f0b7d6d
ravilella <ravilella.1981@gmail.com>
2019-04-24 16:33:02 +0530

    Locators & Logs

--- Durgasoft AfterNoon Batch/src/com/browser/BaseTest.java
+++ Durgasoft AfterNoon Batch/src/com/browser/BaseTest.java
@@ -4,16 +4,21 @@ package com.browser;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class BaseTest 
 {
 	public static WebDriver driver;
 	public static Properties p;
+	public static Properties or;
 	public static FileInputStream fis=null;
 	
 	public static void init() throws Exception
@@ -21,6 +26,13 @@ public class BaseTest
 		p=new Properties();
 		fis=new FileInputStream(System.getProperty("user.dir")+"//data.properties");
 		p.load(fis);
+		
+		or=new Properties();
+		fis=new FileInputStream(System.getProperty("user.dir")+"//OR.properties");
+		or.load(fis);
+		
+		PropertyConfigurator.configure(System.getProperty("user.dir")+"//log4j.properties");
+		
 	}
 	
 	
@@ -46,6 +58,46 @@ public class BaseTest
 	public static void navigate(String url)
 	{
 		driver.get(p.getProperty(url));
+	}
+	
+	
+	public static WebElement getElement(String locatorKey)
+	{
+		WebElement element=null;
+		
+		if(locatorKey.endsWith("_id")) {
+			element=driver.findElement(By.id(or.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_name")) {
+			element=driver.findElement(By.name(or.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_classname")) {
+			element=driver.findElement(By.className(or.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_xpath")) {
+			element=driver.findElement(By.xpath(or.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_css")) {
+			element=driver.findElement(By.cssSelector(or.getProperty(locatorKey)));
+		}
+		return element;
+		
+		
+	}
+	
+	public static void selectOption(String locatorKey, int item) 
+	{
+		WebElement loc = getElement(locatorKey);
+		Select s=new Select(loc);
+		s.selectByIndex(item);
+	}
+	
+	
+	public static void type(String locatorKey, String value) 
+	{
+		getElement(locatorKey).sendKeys(value);		
+	}
+
+	
+	public static void click(String locatorKey) 
+	{
+		getElement(locatorKey).click();
 	}
 
 }
--- Durgasoft AfterNoon Batch/src/com/browser/TC_002.java
+++ Durgasoft AfterNoon Batch/src/com/browser/TC_002.java
@@ -1,14 +1,32 @@
 package com.browser;
 
+import org.apache.log4j.Logger;
+
 public class TC_002 extends BaseTest
 {
+	
+	private static final Logger log=Logger.getLogger(TC_002.class.getName());
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
-		launch("edge");
+		init();
+		log.info("initializion of data & OR & LOg4j  in progress.....");
 		
-		navigate("https://www.bestbuy.com");
-
+		launch("chromebrowser");
+		log.info("Opened Browser :- "+ p.getProperty("chromebrowser"));
+		
+		navigate("amazonurl");
+		log.info("Navigated to URL :- " + p.getProperty("amazonurl"));
+		
+		selectOption("amazondropdown_id",10);
+		log.info("Selected the item 10 By using Locator :- " + or.getProperty("amazondropdown_id"));
+		
+		type("amazonsearchtext_id","Harry Potter");
+		log.info("Entered the text Harry Potter By Using Locator :- " + or.get("amazonsearchtext_id"));
+		
+		click("amazonsearchbutton_xpath");
+		log.info("Clicked on amazon search Button By using Locator :- " + or.getProperty("amazonsearchbutton_xpath"));
+		
 	}
 
 }
